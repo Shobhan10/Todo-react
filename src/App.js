a44d5419e63a7b7f0377b2b2b4ded22cb8f35669
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2022-12-28 21:31:23 +0530

    create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import Alert from "./Alert";
 import List from "./List";
 
+let myTodo = [];
+localStorage.setItem("list", JSON.stringify(myTodo));
+const previousTodo = JSON.parse(localStorage.getItem("list"));
+if (!previousTodo.length) {
+  myTodo = [];
+} else {
+  myTodo = previousTodo;
+}
+
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState(myTodo);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (todo) {
+      myTodo.push({
+        id: Date.now(),
+        title: todo.trim(),
+      });
+    }
+
+    setTodo("");
+    setTodoList(myTodo);
+
+    localStorage.setItem("list", JSON.stringify(myTodo));
+  }
+
   return (
     <section className="section-center">
-      <form className="grocery-form">
+      <form className="grocery-form" onSubmit={handleSubmit}>
         {/* <Alert /> */}
         <h3>Grocery Bud</h3>
         <div className="form-control">
@@ -12,14 +41,15 @@ function App() {
             type="text"
             className="grocery"
             placeholder="e.g. eggs"
-            // value
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
           />
           <button type="submit" className="submit-btn">
             submit
           </button>
         </div>
       </form>
-      <List />
+      <List todoList={todoList} />
     </section>
   );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,24 +1,31 @@
 import { FaEdit, FaTrash } from "react-icons/fa";
 
-const List = () => {
-  return (
-    <div className="grocery-container">
-      <div className="grocery-list">
-        <div className="grocery-item">
-          <p className="title"></p>
-          <div className="btn-container">
-            <button type="button" className="edit-btn">
-              <FaEdit />
-            </button>
-            <button type="button" className="delete-btn">
-              <FaTrash />
-            </button>
-          </div>
+const List = ({ todoList }) => {
+  if (todoList.length) {
+    return (
+      <div className="grocery-container">
+        <div className="grocery-list">
+          {todoList.map((todo) => {
+            const { id, title } = todo;
+            return (
+              <div className="grocery-item" key={id}>
+                <p className="title">{title}</p>
+                <div className="btn-container">
+                  <button type="button" className="edit-btn">
+                    <FaEdit />
+                  </button>
+                  <button type="button" className="delete-btn">
+                    <FaTrash />
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
+        <button className="clear-btn">Clear items</button>
       </div>
-      <button className="clear-btn">Clear items</button>
-    </div>
-  );
+    );
+  }
 };
 
 export default List;
